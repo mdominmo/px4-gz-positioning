4745638b7bfca3e0ae8312e77b3b6eb9de9100c0
mandominguez97 <mandominguez97@gmail.com>
2025-11-26 13:04:09 +0100

    Release 1.0.1

--- src/px4_gz_positioning_node.cpp
+++ src/px4_gz_positioning_node.cpp
@@ -1,22 +1,41 @@
-#include <iostream>
-#include <string>
+/* MIT License
+*
+* Copyright (c) 2025 Manuel Domínguez
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 #include "rclcpp/rclcpp.hpp"
 #include <gz/msgs/pose.pb.h>
 #include <gz/transport/Node.hh>
 #include <px4_msgs/msg/vehicle_odometry.hpp>
 
 
-using namespace std::chrono_literals;
-
 /**
  * @class PX4GZPositioningNode
- * @brief This class bridges the pose from gazebo pose publisher to PX4 Vehicle Visual Odometry topic
+ * @brief This class injects the pose from gazebo pose publisher to PX4 Vehicle Visual Odometry topic
  */
 class PX4GZPositioningNode : public rclcpp::Node {
 public:
   PX4GZPositioningNode() : Node("px4_gz_positioning_node") {
     
-    this->declare_parameter("gz_topic", "/model/x500_mono_cam_1/pose");
+    this->declare_parameter("gz_topic", "/model/x500_vision_1/pose");
 		this->declare_parameter("px4_topic", "/px4_1/fmu/in/vehicle_visual_odometry");
 
     const std::string gz_topic = this->get_parameter("gz_topic").as_string();
@@ -41,13 +60,13 @@ private:
     px4_msg.timestamp = uint64_t(gz_msg.header().stamp().sec())*1000000 + uint64_t(gz_msg.header().stamp().nsec())/1000;
     px4_msg.timestamp_sample = px4_msg.timestamp;
 
-    px4_msg.position[0] = gz_msg.position().x();
-    px4_msg.position[1] = -gz_msg.position().y();
+    px4_msg.position[0] = gz_msg.position().y();
+    px4_msg.position[1] = gz_msg.position().x();
     px4_msg.position[2] = -gz_msg.position().z();
 
     px4_msg.q[0] = gz_msg.orientation().w(); 
-    px4_msg.q[1] = gz_msg.orientation().x(); 
-    px4_msg.q[2] = -gz_msg.orientation().y(); 
+    px4_msg.q[1] = gz_msg.orientation().y(); 
+    px4_msg.q[2] = gz_msg.orientation().x(); 
     px4_msg.q[3] = -gz_msg.orientation().z();
 
     pub_ -> publish(px4_msg);
